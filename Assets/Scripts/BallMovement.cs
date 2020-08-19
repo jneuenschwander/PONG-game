c1445bbf919e20832d2bbd65cd49eb0f5fc68e4f
Juan Antonio <41336919+jneuenschwander@users.noreply.github.com>
2020-08-19 09:10:38 -0500

    Pong casi terminado
    Movimiento de la bola y texto para generar el puntaje.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+/// <summary>
+/// Moves ball within a 2D space for pong game
+/// Author: Sebastian Gomez Rosas
+/// </summary>
 
 public class BallMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 _velocity;
+    [SerializeField] private float movementForce = 1f;
+    
+    private void Awake()
+    
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
-        
+        var direction = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), 0f);
+        _rigidbody2D.AddForce(direction.normalized * movementForce, ForceMode2D.Impulse);
     }
-
-    // Update is called once per frame
     void Update()
     {
+        _velocity = _rigidbody2D.velocity;
+        if (transform.position.x >= 6.25)
+        {
+            transform.position = new Vector3(0f,0f,0f); // aqui deberia enviarse la señal de gano un punto
+        }else if(gameObject.transform.position.x <= -6.25) {
+            transform.position = new Vector3(0f,0f,0f);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var speed = _velocity.magnitude;
+        var newDirection = Vector3.Reflect(_velocity.normalized, other.contacts[0].normal); //lógica del rebote
+        _rigidbody2D.velocity = newDirection * Mathf.Max(speed, 0f);
+        
         
     }
 }
---
+++ Assets/Scripts/ScoreCount.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using TMPro;
+
+
+public class ScoreCount : MonoBehaviour
+{
+    private TMP_Text m_TextComponent;
+
+    private void Awake()
+    {
+        // Referencia al componente instalado
+        m_TextComponent = GetComponent<TMP_Text>();
+    }
+
+    private void Start()
+    {
+        // Cambiar el texto en el componente tmp text
+        m_TextComponent.text = "0   0";
+    }
+}
