9a651e5d3004ceee3be81835982b397c3a154b37
Juan Antonio <41336919+jneuenschwander@users.noreply.github.com>
2020-08-24 18:37:59 -0500

    score system

--- Assets/Scripts/ScoreCount.cs
+++ Assets/Scripts/ScoreCount.cs
@@ -1,20 +1,64 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using TMPro;
 
 
 public class ScoreCount : MonoBehaviour
 {
-    private TMP_Text m_TextComponent;
+    
+    private int _Pad1Score = 10;
+    private int _Pad2Score = 0;
 
+    public int Pad1Score
+    {
+        get => _Pad1Score;
+        set => _Pad1Score = value;
+    }
+
+    public int Pad2Score
+    {
+        get => _Pad2Score;
+        set => _Pad2Score = value;
+    }
+
+    public GameObject Ball
+    {
+        get => _ball;
+        set => _ball = value;
+    }
+
+    private TextMeshProUGUI _uiText;
+    private GameObject _ball;
     private void Awake()
     {
         // Referencia al componente instalado
-        m_TextComponent = GetComponent<TMP_Text>();
+        _uiText = GetComponent<TextMeshProUGUI>();
+        _uiText.text = "0   0";
+        _ball = GameObject.Find("Ball");
     }
 
-    private void Start()
+    private void Update()
     {
-        // Cambiar el texto en el componente tmp text
-        m_TextComponent.text = "0   0";
+        if (_Pad1Score < 10 && _Pad2Score < 10)
+        {
+            if (_ball.transform.position.x >= 6.25)
+            {
+                _Pad1Score++;
+                _uiText.text = _Pad1Score.ToString() + "   " + Pad2Score.ToString();
+            }
+            else if (_ball.gameObject.transform.position.x <= -6.25)
+            {
+                _Pad2Score++;
+                _uiText.text = _Pad1Score.ToString() + "   " + Pad2Score.ToString();
+            }
+        }else if (_Pad1Score == 10)
+        {
+            _uiText.text = "Player 1 wins!!";
+        }
+        else if (_Pad2Score == 10)
+        {
+            _uiText.text = "Player 2 wins!!";
+        }
+    
     }
 }
